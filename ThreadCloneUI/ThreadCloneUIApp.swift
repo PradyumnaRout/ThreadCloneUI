b8d8f9843f7da0f232c5bf451f0c98327e9f372e
Pradyumna Rout <pradyumnarout1433@gmail.com>
2025-09-10 13:46:50 +0530

    Initial Commit

---
+++ ThreadCloneUI/ThreadCloneUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadCloneUIApp.swift
+//  ThreadCloneUI
+//
+//  Created by hb on 10/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ThreadCloneUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
