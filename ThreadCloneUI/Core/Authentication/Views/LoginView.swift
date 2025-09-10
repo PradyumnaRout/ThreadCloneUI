48e9f9b09475f021e9ebc3e78d0bed6f51cbc3f0
Pradyumna Rout <pradyumnarout1433@gmail.com>
2025-09-10 13:53:34 +0530

    Folder Structure

---
+++ ThreadCloneUI/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  ThreadCloneUI
+//
+//  Created by hb on 10/09/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
